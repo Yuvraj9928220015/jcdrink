dde66f5dbcc4801fab1c4e0945df2045ef54e574
Yuvraj9928220015 <yuvrajsharma190105@gmail.com>
2025-11-20 16:10:23 +0530

    Updated project files

--- Backend/index.js
+++ Backend/index.js
@@ -12,16 +12,25 @@ const authRoutes = require('./routes/authRoutes');
 dotenv.config();
 const app = express();
 
+// app.use(cors({
+//     origin: [
+//         'http://localhost:5173',
+//         'http://localhost:5174',
+//         'http://localhost:3000',
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
 app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false
 }));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
