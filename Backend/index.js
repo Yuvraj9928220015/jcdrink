7a5183bf92c9fb271ac90ef924eec0863895faa2
Yuvraj9928220015 <yuvrajsharma190105@gmail.com>
2026-01-22 16:38:24 +0530

    Add Subtitle

--- Backend/index.js
+++ Backend/index.js
@@ -14,9 +14,12 @@ const app = express();
 
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:3000",
+        "https://jcdrink.com",
+        "https://www.jcdrink.com",
+        "https://api.jcdrink.com"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
